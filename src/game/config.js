0d390aa0cc86944146dcd51765cc6c79130bca07
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-10-20 12:04:05 +0300

    init

---
+++ src/game/config.js
@@ -0,0 +1,10 @@
+pandaConfig = {
+    name: 'Pandamark',
+    
+    system: {
+        width: 800,
+        height: 600,
+        webGL: true,
+        bgColor: '#000000'
+    }
+};
---
+++ src/game/main.js
@@ -0,0 +1,100 @@
+game.module(
+    'game.main'
+)
+.body(function() {
+
+game.addAsset('panda.png');
+
+game.createScene('Main', {
+    backgroundColor: 0xffffff,
+    startPandaCount: 2,
+    gravity: 0.75,
+    pandas: [],
+    minX: 0,
+    minY: 0,
+
+    init: function() {
+        this.maxX = game.system.width;
+        this.maxY = game.system.height;
+
+        this.addAmount = game.system.renderer.gl ? 50 : 5;
+            
+        if (this.addAmount === 5) {
+            game.system.renderer.context.mozImageSmoothingEnabled = false;
+            game.system.renderer.context.webkitImageSmoothingEnabled = false;
+        }
+
+        this.pandaCount = document.createElement('div');
+        this.pandaCount.style.position = 'absolute';
+        this.pandaCount.style.left = '10px';
+        this.pandaCount.style.top = '30px';
+        this.pandaCount.style.fontFamily = 'Arial';
+        this.pandaCount.style.fontSize = '20px';
+        this.pandaCount.style.color = 'green';
+        document.body.appendChild(this.pandaCount);
+
+        for (var i = 0; i < this.startPandaCount; i++) this.addPanda();
+    },
+
+    addPanda: function() {
+        var panda = new game.Sprite('panda.png');
+        panda.speedX = Math.random() * 10;
+        panda.speedY = Math.random() * 10 - 5;
+        
+        panda.anchor.set(0.5, 1);
+        panda.position.set(panda.width / 2, panda.height);
+        this.pandas.push(panda);
+        this.stage.addChild(panda);
+
+        this.pandaCount.innerHTML = this.pandas.length + ' PANDAS';
+    },
+
+    mousedown: function() {
+        this.isAdding = true;
+    },
+
+    mouseup: function() {
+        this.isAdding = false;
+    },
+
+    update: function() {
+        var i;
+        var panda;
+
+        if (this.isAdding) {
+            for (i = 0; i < this.addAmount; i++) this.addPanda();
+        }
+
+        for (i = 0; i < this.pandas.length; i++) {
+            panda = this.pandas[i];
+            
+            panda.position.x += panda.speedX * game.system.delta * 60;
+            panda.position.y += panda.speedY * game.system.delta * 60;
+            panda.speedY += this.gravity * game.system.delta * 60;
+
+            if (panda.position.x + panda.width / 2 > this.maxX) {
+                panda.speedX *= -1;
+                panda.position.x = this.maxX - panda.width / 2;
+            }
+            else if (panda.position.x - panda.width / 2 < this.minX) {
+                panda.speedX *= -1;
+                panda.position.x = this.minX + panda.width / 2;
+            }
+            if (panda.position.y > this.maxY) {
+                panda.speedY *= -0.85;
+                panda.position.y = this.maxY;
+                if (Math.random() > 0.5) panda.speedY -= Math.random() * 6;
+            }
+            else if (panda.position.y - panda.height < this.minY) {
+                panda.speedY = 0;
+                panda.position.y = this.minY + panda.height;
+            }
+        }
+
+        this._super();
+    }
+});
+
+game.Debug.enabled = true;
+
+});
